c60ab5a3ae9d0334227a0de93a6d1d0f7672be50
alfaalexx <alfaalex02@gmail.com>
2023-11-19 14:13:27 +0700

    membuat upload payment receipt image

--- lib/model/payment_model.dart
+++ lib/model/payment_model.dart
@@ -1,29 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Payment {
+  final String id; // Tambahkan properti ID
   final String userUid;
   final String reservationId;
   final double warehousePrice;
   final String paymentMethod;
   final String status;
+  String? paymentReceiptImageUrl; // Buat properti tidak final
 
   Payment({
+    required this.id,
     required this.userUid,
     required this.reservationId,
     required this.warehousePrice,
     required this.paymentMethod,
     required this.status,
+    this.paymentReceiptImageUrl,
   });
 
   // Tambahkan konstruktor dari dokumen snapshot
   factory Payment.fromSnapshot(DocumentSnapshot snapshot) {
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
     return Payment(
+      id: snapshot.id, // Ambil ID dari snapshot
       userUid: data['userUid'],
       reservationId: data['reservationId'],
       warehousePrice: data['warehousePrice'],
       paymentMethod: data['paymentMethod'],
       status: data['status'],
+      paymentReceiptImageUrl: data['paymentReceiptImageUrl'],
     );
   }
 }
--- lib/pages/payment/payment_warehouse_page.dart
+++ lib/pages/payment/payment_warehouse_page.dart
@@ -3,9 +3,14 @@ import 'package:warebox_seller/model/reservation_model.dart';
 import 'package:warebox_seller/model/payment_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart'; // Import CachedNetworkImage
 
 class PaymentWarehousePage extends StatefulWidget {
   final String reservationID;
+
   const PaymentWarehousePage({Key? key, required this.reservationID})
       : super(key: key);
 
@@ -16,6 +21,7 @@ class PaymentWarehousePage extends StatefulWidget {
 class _PaymentWarehousePageState extends State<PaymentWarehousePage> {
   Reservation? reservation;
   Payment? payment;
+  bool isUploading = false;
 
   @override
   void initState() {
@@ -29,7 +35,6 @@ class _PaymentWarehousePageState extends State<PaymentWarehousePage> {
 
   Future<void> loadReservationAndPaymentData() async {
     try {
-      // Mengambil data reservation berdasarkan reservationID
       DocumentSnapshot reservationSnapshot = await FirebaseFirestore.instance
           .collection('reservations')
           .doc(widget.reservationID)
@@ -41,7 +46,6 @@ class _PaymentWarehousePageState extends State<PaymentWarehousePage> {
         });
       }
 
-      // Mengambil data payment berdasarkan reservationID
       QuerySnapshot paymentSnapshot = await FirebaseFirestore.instance
           .collection('payments')
           .where('reservationId', isEqualTo: widget.reservationID)
@@ -57,6 +61,73 @@ class _PaymentWarehousePageState extends State<PaymentWarehousePage> {
     }
   }
 
+  Future<void> uploadPaymentReceiptImage(File imageFile) async {
+    try {
+      setState(() {
+        isUploading = true;
+      });
+
+      String storagePath =
+          'payment_receipts/${getCurrentUserUid()}/${widget.reservationID}/${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+      Reference storageReference =
+          FirebaseStorage.instance.ref().child(storagePath);
+
+      await storageReference.putFile(imageFile);
+
+      String downloadUrl = await storageReference.getDownloadURL();
+
+      await FirebaseFirestore.instance
+          .collection('payments')
+          .doc(payment?.id)
+          .update({
+        'paymentReceiptImageUrl': downloadUrl,
+        'status': 'Waiting for payment verification from WareBox Admin',
+        'paidDate': Timestamp.now()
+      });
+
+      await FirebaseFirestore.instance
+          .collection('reservations')
+          .doc(reservation?.id)
+          .update({
+        'paymentStatus': 'Waiting for payment verification from WareBox Admin',
+      });
+
+      setState(() {
+        payment?.paymentReceiptImageUrl = downloadUrl;
+        isUploading = false;
+      });
+
+      // Reload the data to get the latest changes
+      await loadReservationAndPaymentData();
+    } catch (e) {
+      print('Error uploading payment receipt image: $e');
+      setState(() {
+        isUploading = false;
+      });
+    }
+  }
+
+  Future<void> pickImageAndUpload() async {
+    final ImagePicker _picker = ImagePicker();
+
+    try {
+      final XFile? pickedImage = await _picker.pickImage(
+        source: ImageSource.gallery,
+      );
+
+      if (pickedImage != null) {
+        File imageFile = File(pickedImage.path);
+        await uploadPaymentReceiptImage(imageFile);
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+      setState(() {
+        isUploading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     User? currentUser = FirebaseAuth.instance.currentUser;
@@ -68,51 +139,66 @@ class _PaymentWarehousePageState extends State<PaymentWarehousePage> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Reservation Details',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              if (reservation != null) ...[
-                Text('Warehouse ID: ${reservation!.warehouseId}'),
-                Text('Duration Type: ${reservation!.durationType}'),
-                Text('User UID: ${reservation!.userUid}'),
-                Text('Status: ${reservation!.status}'),
-                Text('Is Paid: ${reservation!.isPaid}'),
-                Text('Payment Status: ${reservation!.paymentStatus}'),
-                SizedBox(height: 20),
-              ] else
-                Text('Reservation data not available'),
-              Container(
-                margin: EdgeInsets.only(top: 20.0),
-                child: Text(
-                  'Payment Details',
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Reservation Details',
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              if (payment != null) ...[
-                Text('User UID: ${payment!.userUid}'),
-                Text('Reservation ID: ${payment!.reservationId}'),
-                Text('Warehouse Price: ${payment!.warehousePrice}'),
-                Text('Payment Method: ${payment!.paymentMethod}'),
-                Text('Status: ${payment!.status}'),
-              ] else
-                Text('Payment data not available'),
-            ],
+                if (reservation != null) ...[
+                  Text('Warehouse ID: ${reservation!.warehouseId}'),
+                  Text('Duration Type: ${reservation!.durationType}'),
+                  Text('User UID: ${reservation!.userUid}'),
+                  Text('Status: ${reservation!.status}'),
+                  Text('Is Paid: ${reservation!.isPaid}'),
+                  Text('Payment Status: ${reservation!.paymentStatus}'),
+                  SizedBox(height: 20),
+                ] else
+                  Text('Reservation data not available'),
+                Container(
+                  margin: EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    'Payment Details',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                if (payment != null) ...[
+                  Text('User UID: ${payment!.userUid}'),
+                  Text('Reservation ID: ${payment!.reservationId}'),
+                  Text('Warehouse Price: ${payment!.warehousePrice}'),
+                  Text('Payment Method: ${payment!.paymentMethod}'),
+                  Text('Status: ${payment!.status}'),
+                  if (payment?.paymentReceiptImageUrl != null)
+                    CachedNetworkImage(
+                      imageUrl: payment!.paymentReceiptImageUrl!,
+                      placeholder: (context, url) =>
+                          CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                  ElevatedButton(
+                    onPressed:
+                        (isUploading || payment?.paymentReceiptImageUrl != null)
+                            ? null
+                            : pickImageAndUpload,
+                    child: Text('Upload Payment Receipt'),
+                  ),
+                  if (isUploading) CircularProgressIndicator(),
+                ] else
+                  Text('Payment data not available'),
+              ],
+            ),
           ),
         ),
       );
     } else
-      // Handle unauthorized access, for example, redirect to another page
-      // You can replace the code below with your own logic
       return Scaffold(
         body: Center(
           child: Text('Unauthorized Access'),
