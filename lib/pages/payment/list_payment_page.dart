49e995b03dc66072dccf115f1fc108133cd0172c
alfaalexx <alfaalex02@gmail.com>
2023-11-21 17:59:02 +0700

    revisi ui recent payments

--- lib/pages/payment/list_payment_page.dart
+++ lib/pages/payment/list_payment_page.dart
@@ -3,7 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:warebox_seller/model/payment_model.dart';
 import 'package:cached_network_image/cached_network_image.dart'; // Import CachedNetworkImage
-// Import model pembayaran
+import 'package:google_fonts/google_fonts.dart';
+import 'package:warebox_seller/utils/custom_themes.dart';
+import 'package:intl/intl.dart';
 
 class MyPaymentPage extends StatefulWidget {
   @override
@@ -13,11 +15,26 @@ class MyPaymentPage extends StatefulWidget {
 class _MyPaymentPageState extends State<MyPaymentPage> {
   User? currentUser = FirebaseAuth.instance.currentUser;
 
+  String formatRupiah(double value) {
+    final formatter = NumberFormat.currency(
+        locale: 'id_ID', symbol: 'Rp. ', decimalDigits: 0);
+    return formatter.format(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Payments'),
+        title: Text(
+          'Recent Payments',
+          textAlign: TextAlign.start,
+          style: GoogleFonts.plusJakartaSans(
+              fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       body: currentUser != null
           ? StreamBuilder(
@@ -37,23 +54,68 @@ class _MyPaymentPageState extends State<MyPaymentPage> {
                       itemCount: payments.length,
                       itemBuilder: (context, index) {
                         Payment payment = payments[index];
-                        return Card(
-                          child: ListTile(
-                            title: Text(
-                                'Reservation ID: ${payment.reservationId}'),
-                            subtitle: Text(
-                                'Warehouse Price: ${payment.warehousePrice}'),
-                            onTap: () {
-                              // Navigate to payment detail page with payment data
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => DetailPaymentPage(
-                                    payment: payment,
-                                  ),
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailPaymentPage(
+                                  payment: payment,
                                 ),
-                              );
-                            },
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              margin: EdgeInsets.symmetric(vertical: 5.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Color(0xFFE5E5E5),
+                                ),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Reservation ID: ${payment.reservationId}',
+                                            style: pjsMedium18,
+                                          ),
+                                          SizedBox(height: 8),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Payment Method: ${payment.paymentMethod}',
+                                                style: TextStyle(
+                                                    color: Colors.grey[600]),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Text(
+                                                  '${formatRupiah(payment.warehousePrice)}',
+                                                  style: pjsSemiBold14Green,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(height: 4),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
                         );
                       },
@@ -86,29 +148,105 @@ class DetailPaymentPage extends StatelessWidget {
 
   const DetailPaymentPage({Key? key, required this.payment}) : super(key: key);
 
+  String formatRupiah(double value) {
+    final formatter = NumberFormat.currency(
+        locale: 'id_ID', symbol: 'Rp. ', decimalDigits: 0);
+    return formatter.format(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Payment Details'),
+        title: Text(
+          'Payment Details',
+          textAlign: TextAlign.start,
+          style: GoogleFonts.plusJakartaSans(
+              fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Reservation ID: ${payment.reservationId}'),
-            Text('Warehouse Price: ${payment.warehousePrice}'),
-            Text('Payment Method: ${payment.paymentMethod}'),
-            Text('Status: ${payment.status}'),
-            if (payment.paymentReceiptImageUrl != null)
-              CachedNetworkImage(
-                imageUrl: payment.paymentReceiptImageUrl!,
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 12),
+                child: Text(
+                  'Payment Details',
+                  style: pjsMedium16,
+                ),
               ),
-            // Add more details as needed
-          ],
+
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Warehouse Price',
+                      style: pjsSemiBold14Ls400Grey,
+                    ),
+                    Text(
+                      '${formatRupiah(payment.warehousePrice)}',
+                      style: pjsSemiBold14,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Service Fee (1%)',
+                      style: pjsSemiBold14Ls400Grey,
+                    ),
+                    Text(
+                      '${formatRupiah(payment.serviceFee)}',
+                      style: pjsSemiBold14,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Amount',
+                      style: pjsSemiBold14Ls400Grey,
+                    ),
+                    Text(
+                      '${formatRupiah(payment.totalAmount)}',
+                      style: pjsMedium18,
+                    ),
+                  ],
+                ),
+              ),
+              if (payment.paymentReceiptImageUrl != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CachedNetworkImage(
+                    imageUrl: payment.paymentReceiptImageUrl!,
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
+                ),
+
+              // Add more details as needed
+            ],
+          ),
         ),
       ),
     );
